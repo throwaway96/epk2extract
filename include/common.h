86ba8d52adeaa26f439b61d29385ee537bf9072c
throwaway96 <68320646+throwaway96@users.noreply.github.com>
2023-12-31 12:43:48 -0500

    add int_sizeof() macro and use in epk3.c

--- include/common.h
+++ include/common.h
@@ -34,4 +34,13 @@
 
 #define countof(x) (sizeof(x) / sizeof((x)[0]))
 
+#ifdef __GNUC__
+#define int_sizeof(type) ({ \
+	static_assert(sizeof(type) < INT_MAX, "size of " #type " too big to fit in int"); \
+	(int) sizeof(type); \
+})
+#else
+#define int_sizeof(type) ((int) sizeof(type))
+#endif
+
 #endif
