748d5b1655c9aa9ed947c1f5621f18a0338c677b
throwaway96 <68320646+throwaway96@users.noreply.github.com>
2024-07-21 15:04:57 -0400

    use const parameters in decompression handlers

--- include/minigzip.h
+++ include/minigzip.h
@@ -3,13 +3,13 @@
 #include <zlib.h>
 
 void error(const char *msg);
-void gz_compress(FILE * in, gzFile out);
+void gz_compress(FILE *in, gzFile out);
 #ifdef USE_MMAP
-int gz_compress_mmap(FILE * in, gzFile out);
+int gz_compress_mmap(FILE *in, gzFile out);
 #endif
-void gz_uncompress(gzFile in, FILE * out);
-void file_compress(char *file, char *mode);
-void file_uncompress(char *infile, char *outfile);
-char *file_uncompress_origname(char *infile, char *path);
+void gz_uncompress(gzFile in, FILE *out);
+void file_compress(const char *file, const char *mode);
+void file_uncompress(const char *file);
+char *file_uncompress_origname(const char *infile, const char *path);
 
 #endif /* MINIGZIP_H */
